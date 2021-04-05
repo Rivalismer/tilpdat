ef6ba95bac982e8cfd26b57bc0c4f05e0f444a72
Vickenwo <victortk@stud.ntnu.no>
2021-04-05 15:32:05 +0200

    We did it bois, number 2 down

--- lab-3-mikrokontroller/2_uart/main.c
+++ lab-3-mikrokontroller/2_uart/main.c
@@ -1,11 +1,23 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "gpio.h"
 #include "uart.h"
 
 #define __BUTTON_A_PIN__	17
 #define __BUTTON_B_PIN__	26
 
+ssize_t _write(int fd, const void *buf, size_t count){
+	char *letter = (char *)(buf);
+	for (int i = 0; i < count; i++)
+	{
+		uart_send(*letter);
+		letter++;
+	}
+	return count;
+}
+
 int main(){
+	uart_init();
 	// Configure LED Matrix
 	for (int i = 4; i <= 15; i++)
 	{
@@ -18,12 +30,15 @@ int main(){
 	GPIO->PIN_CNF[__BUTTON_B_PIN__] = 0;
 
 	int sleep = 0;
+	uint8_t on = 0;
 	while (1)
 	{
-
+		if(uart_read() != '\0'){
+			on = !on;
+		}
 		/* Check if button B is pressed;
 		 * turn on LED matrix if it is. */
-		if (1 << __BUTTON_B_PIN__ & ~GPIO->IN)
+		if (on)
 		{
 			for (int i = 13; i <= 15; i++)
 			{
@@ -35,10 +50,9 @@ int main(){
 				GPIO->OUTCLR = (1 << i);
 			}
 		}
-
 		/* Check if button A is pressed;
 		 * turn off LED matrix if it is. */
-		if (1 << __BUTTON_A_PIN__ & ~GPIO->IN)
+		else if (!on)
 		{
 			for (int i = 13; i <= 15; i++)
 			{
@@ -48,6 +62,15 @@ int main(){
 			{
 				GPIO->OUTSET = (1 << i);
 			}
+		}
+
+		if (1 << __BUTTON_A_PIN__ & ~GPIO->IN){
+			uart_send('A');
+			iprintf("\nThe average grade in TTK%d was in %d and %d: %c\n\r", 4235, 2019, 2018, 'C');
+		}
+
+		if (1 << __BUTTON_B_PIN__ & ~GPIO->IN) {
+			uart_send('B');
 		}
 
 		sleep = 10000;
--- lab-3-mikrokontroller/2_uart/uart.c
+++ lab-3-mikrokontroller/2_uart/uart.c
@@ -2,14 +2,38 @@
 #include "gpio.h"
 
 void uart_init(){
+
     GPIO->PIN_CNF[PIN_RXD] = 0;
-    GPIO->PIN_CNF[PIN_TXD] = 0x1;
-    UART->INTEN = 0;
+    GPIO->PIN_CNF[PIN_TXD] = 1;
+    UART->INTENCLR = 0xFFFFFFFF;
     UART->PSELRTS = 0xFFFFFFFF;
     UART->PSELCTS = 0xFFFFFFFF;
-    UART->PSELTXD = 1 << PIN_TXD;
-    UART->PSELRXD = 1 << PIN_RXD;
+    UART->PSELTXD = PIN_TXD;
+    UART->PSELRXD = PIN_RXD;
     UART->BAUDRATE = 0x00275000;
     UART->ENABLE = 4;
     UART->STARTRX = 1;
+}
+
+void uart_send(char letter){
+    UART->STARTTX = 1;
+    UART->TXDRDY = 0;
+    UART->TXD = letter;
+
+    while(!(UART->TXDRDY)){ }
+    
+    UART->TXDRDY = 0;
+    UART->STOPTX = 1;
+}
+
+
+char uart_read(){
+    UART->STARTRX = 1;
+    
+    if(UART->RXDRDY){
+        UART->RXDRDY = 0;
+        return UART->RXD;
+    }
+
+    return '\0';
 }
